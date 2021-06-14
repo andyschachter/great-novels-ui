545039eaf8cd487f123f463f00558e3fa8f67c5c
Andy Schachter <andy.schachter@gmail.com>
2021-06-13 23:01:17 -0400

    began adding state to app.js

--- great-novels-app/src/App.js
+++ great-novels-app/src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import search from './components/novelSearch'
+import { fetchData, filtered } from './utils/novels'
 
 function App() {
+
+  const [novelData, setNovelData] = useState([])
+  const [foundNovels, setFoundNovels] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    async function pullData() {
+
+      const data = await fetchData()
+
+      setNovelData(data)
+      setFoundNovels(data)
+    }
+
+    pullData()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
